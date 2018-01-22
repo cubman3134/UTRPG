d1032c177f3f990e9e45eae92b3dc32ed46b3990
Parker Holt <cubman3134@gmail.com>
2018-01-22 08:25:04 -0500

    added height changes
    your height now changes whenever you move!

--- Assets/CharController.cs
+++ Assets/CharController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 public class CharController : MonoBehaviour
 {
@@ -9,13 +10,35 @@ public class CharController : MonoBehaviour
     float moveSpeed = 6f;
     Vector3 forward, right;
     bool needToCheckMovement;
-    public GameObject[] groundObjects;
+    //public GameObject[] groundObjects;
+    public Dictionary<Vector3, GameObject> touchingObjects;
+    public GameObject test;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //touchingObjects[test.GetComponent<Map>().map[new Vector2(other.gameObject.transform.position.x, other.gameObject.transform.position.z)].transform.position]
+        //    = test.GetComponent<Map>().map[new Vector2(other.gameObject.transform.position.x, other.gameObject.transform.position.z)];
+        //Debug.Log(touchingObjects.Count);
+
+        //GameObject g = other.gameObject;
+        //touchingObjects[g.transform.position] = g;
+        touchingObjects[other.gameObject.transform.position] = other.gameObject;
+        Debug.Log(touchingObjects.Count);
+        //Debug.Log(other.gameObject.gameObject.transform.position.x + " " + other.gameObject.gameObject.transform.position.y + " " + other.gameObject.gameObject.transform.position.z);
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        touchingObjects.Remove(other.gameObject.transform.position);
+        //touchingObjects.Remove(test.GetComponent<Map>().map[new Vector2(other.gameObject.transform.position.x, other.gameObject.transform.position.z)].transform.position);
+    }
 
     void Start()
     {
         needToCheckMovement = false;
         forward = Camera.main.transform.forward;
-        
+        touchingObjects = new Dictionary<Vector3, GameObject>();
         forward.y = 0;
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
@@ -25,16 +48,40 @@ public class CharController : MonoBehaviour
     {
         if (Input.anyKey)
         {
-            GameObject closest = findClosestGroundObject();
+            //GameObject closest = findClosestGroundObject(test.GetComponent<Map>().map);
             Move();
+            MoveY();
             needToCheckMovement = true;
         }
         else if (needToCheckMovement == true)
         {
-            GameObject closest = findClosestGroundObject();
+            GameObject closest = findClosestGroundObject(test.GetComponent<Map>().map);
             moveTowards(closest);
             //Debug.Log(closest.name);
             needToCheckMovement = false;
+        }
+    }
+
+    void MoveY()
+    {
+        GameObject closest = findClosestGroundObject(test.GetComponent<Map>().map);
+        //GameObject closest = findClosestGroundObject(touchingObjects.Values.ToList());
+        if (closest)
+        {
+            //if((closest.transform.position.y + .5)> this.gameObject.transform.position.y)
+            //{
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, (float)(closest.transform.position.y + 1.5), transform.position.z), 5);
+                //transform.position += new Vector3(0, closest.transform.position.y - this.gameObject.transform.position.y);
+            //} else if(transform.position.y > (closest.transform.position.y + .5))
+            //{
+                //transform.position -= new Vector3(0, this.gameObject.transform.position.y - closest.transform.position.y);
+            //}
+        } else
+        {
+            if(transform.position.y != 1.5)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, (float)1.5, transform.position.y), 5);
+            }
         }
     }
 
@@ -52,7 +99,6 @@ public class CharController : MonoBehaviour
             rightMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey") * Camera.main.GetComponent<CameraFollow>().horiz;
             upMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey") * Camera.main.GetComponent<CameraFollow>().vert;
         }
-
         Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
 
         transform.forward = heading;
@@ -68,29 +114,32 @@ public class CharController : MonoBehaviour
         return Math.Sqrt(Math.Pow(rend2.bounds.center.x - rend1.bounds.center.x, 2) + Math.Pow(rend2.bounds.center.z - rend1.bounds.center.z, 2));
     }
 
-    GameObject findClosestGroundObject()
+    GameObject findClosestGroundObject(List<GameObject> gameObjectsList)
     {
         //Debug.Log(groundObjects.Length);
-        if (groundObjects.Length == 0)
+        if (gameObjectsList.Count == 0)
         {
-            groundObjects = GameObject.FindGameObjectsWithTag("GroundObject");
+            return null;
+            //groundObjects = GameObject.FindGameObjectsWithTag("GroundObject");
         }
         double distance;
-        double bestDistance = distanceCalc(this.gameObject, groundObjects[0]);
-        GameObject closest = groundObjects[0];
+        double bestDistance = distanceCalc(this.gameObject, gameObjectsList[0]);
+        GameObject closest = gameObjectsList[0];
         CapsuleCollider playerCollider = this.gameObject.GetComponent<CapsuleCollider>();
-        for (int i = 1; i < groundObjects.Length; i++)
+        for (int i = 1; i < gameObjectsList.Count; i++)
         {
             //if (playerCollider.IsTouching(groundObjects[i].GetComponent<Collider2D>())) {
-            distance = distanceCalc(this.gameObject, groundObjects[i]);
+            distance = distanceCalc(this.gameObject, gameObjectsList[i]);
             if (distance < bestDistance)
             {
                 bestDistance = distance;
-                closest = groundObjects[i];
+                closest = gameObjectsList[i];
             }
         }
         return closest;
     }
+    
+    
 
     void moveTowards(GameObject target)
     {
@@ -98,7 +147,7 @@ public class CharController : MonoBehaviour
         float targetRad = target.transform.position.z / 2;
         Renderer playerRend = this.gameObject.GetComponent<Renderer>();
         Renderer targetRend = target.GetComponent<Renderer>();
-        Vector3 desiredPosition = new Vector3(targetRend.bounds.center.x, this.gameObject.transform.position.y, targetRend.bounds.center.z);
+        Vector3 desiredPosition = new Vector3(targetRend.bounds.center.x, (float)(target.transform.position.y + 1.5), targetRend.bounds.center.z);
         while (Input.anyKey == false && this.gameObject.transform.position != desiredPosition)
         {
             this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, desiredPosition, moveSpeed * Time.deltaTime);
--- Assets/Map.cs
+++ Assets/Map.cs
@@ -6,13 +6,17 @@ public class Map : MonoBehaviour
 {
     public GameObject[] tiles;
     public List<GameObject> map;
+    //public Dictionary<Vector2, GameObject> map;
+
 
     int tilesetSizes = 4;
     void Start()
     {
+        //map = new Dictionary<Vector2, GameObject>();
+        map = new List<GameObject>();
         GenerateMap(10, 1, 4);
         //tilesets
-        //map = new List();
+        
     }
     /*
      * size refers to the size of the map.
@@ -30,14 +34,17 @@ public class Map : MonoBehaviour
             {
                 if (curX != 0 && curZ != 0)
                 {
+                    //curY = (int)((map[new Vector2(curX - 1, curZ)].transform.position.y + map[new Vector2(curX, curZ - 1)].transform.position.y) / 2);
                     curY = (int)(map[map.Count - size].transform.position.y + map[map.Count - 1].transform.position.y) / 2;
                 }
                 else if (curX != 0)
                 {
+                    //curY = (int)(map[new Vector2(curX - 1, curZ)].transform.position.y);
                     curY = (int)map[map.Count - size].transform.position.y;
                 }
                 else if (curZ != 0)
                 {
+                    //curY = (int)(map[new Vector2(curX, curZ - 1)].transform.position.y);
                     curY = (int)map[map.Count - 1].transform.position.y;
                 }
                 if (hills >= 0)
@@ -67,14 +74,30 @@ public class Map : MonoBehaviour
                     }
                 }
                 int curTileRand = Random.Range(0 + (tileset * tilesetSizes), tilesetSizes + (tileset * tilesetSizes));
+                //map[new Vector2(curX, curZ)] = Instantiate(tiles[curTileRand], new Vector3(curX, curY, curZ), transform.rotation);
                 map.Add(Instantiate(tiles[curTileRand], new Vector3(curX, curY, curZ), transform.rotation));
                 for (int i = 0; i < curY; i++)
                 {
                     Instantiate(tiles[curTileRand], new Vector3(curX, i, curZ), transform.rotation);
                 }
+
                 map[map.Count - 1].AddComponent<BoxCollider>();
+                map[map.Count - 1].GetComponent<BoxCollider>().center = new Vector3((float)-.5, (float).55, (float).5);
+                map[map.Count - 1].GetComponent<BoxCollider>().size = new Vector3(1, (float)1.1, 1);
+                //map[map.Count - 1].GetComponent<BoxCollider>().isTrigger = true;
                 map[map.Count - 1].tag = "GroundObject";
-                map[map.Count - 1] = Instantiate(tiles[curTileRand], new Vector3(curX, curY, curZ), transform.rotation);
+
+
+                //GameObject currentGameObject = map[new Vector2(curX, curZ)];
+                //currentGameObject.AddComponent<BoxCollider>();
+                //currentGameObject.GetComponent<BoxCollider>().center = new Vector3((float)-.5, (float).55, (float).5);
+                //currentGameObject.GetComponent<BoxCollider>().size = new Vector3(1, (float)1.1, 1);
+                //currentGameObject.tag = "GroundObject";
+
+
+                //map[map.Count - 1].AddComponent<Rigidbody>();
+                //map[map.Count - 1].GetComponent<Rigidbody>().isKinematic = true;
+                //map[map.Count - 1] = Instantiate(tiles[curTileRand], new Vector3(curX, curY, curZ), transform.rotation);
             }
         }
     }
@@ -89,10 +112,10 @@ public class Map : MonoBehaviour
     //doesn't currently work correctly
     public void DestroyMap()
     {
-        for(int i = map.Count - 1; i >= 0; i--)
-        {
-            GameObject g = map[i];
-            Destroy(g);
-        }
+        //for(int i = map.Count - 1; i >= 0; i--)
+        //{
+        //    GameObject g = map[i];
+        //    Destroy(g);
+        //}
     }
 }
